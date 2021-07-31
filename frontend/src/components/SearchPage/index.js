44391f52d55ac05c65275b5ecb479084f4d1ac32
Joe Kim <joekim6a@gmail.com>
2021-07-30 19:14:22 -0700

    finished search!

--- frontend/src/components/SearchPage/index.js
+++ frontend/src/components/SearchPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import { getPlayLists } from '../../store/playlist';
@@ -11,10 +11,24 @@ import NavigationSide from '../NavigationSide'
 import './SearchPage.css'
 
 function SearchPage() {
+    const [searchValue, setSearchValue] = useState('')
+
     const playlists = useSelector((state) => Object.values(state.playlists))
     const artists = useSelector((state) => Object.values(state.artists))
     const albums = useSelector((state) => Object.values(state.albums))
     const songs = useSelector((state) => Object.values(state.songs))
+
+    let searchPlaylists;
+    let searchArtists;
+    let searchAlbums;
+    let searchSongs;
+
+    if (searchValue) {
+        searchPlaylists = playlists?.filter(playlist => (playlist.name.toLowerCase().includes(searchValue.toLowerCase())))
+        searchArtists = artists?.filter(artist => (artist.name.toLowerCase().includes(searchValue.toLowerCase())))
+        searchAlbums = albums?.filter(album => (album.name.toLowerCase() == searchValue.toLowerCase()))
+        searchSongs = songs?.filter(song => (song.name.toLowerCase() == searchValue.toLowerCase()))
+    }
 
     const dispatch = useDispatch();
 
@@ -26,7 +40,7 @@ function SearchPage() {
     }, [dispatch]);
 
     const searchNav = (
-        <input placeholder="Search by playlist, artist, album, or song" />
+        <input onChange={(e) => setSearchValue(e.target.value)} placeholder="Search by playlist, artist, album, or song" />
     )
 
     return (
@@ -35,9 +49,21 @@ function SearchPage() {
             <div id='search__content'>
                 <NavigationTop searchNav={searchNav}/>
                 <div className='search__header'>Playlists</div>
+                {searchPlaylists?.map(playlist => (
+                    <div>{playlist.name}</div>
+                ))}
                 <div className='search__header'>Artists</div>
+                {searchArtists?.map(artist => (
+                    <div>{artist.name}</div>
+                ))}
                 <div className='search__header'>Albums</div>
+                {searchAlbums?.map(album => (
+                    <div>{album.name}</div>
+                ))}
                 <div className='search__header'>Songs</div>
+                {searchSongs?.map(song => (
+                    <div>{song.name}</div>
+                ))}
             </div>
         </div>
     )
